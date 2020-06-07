5da81ecf6109585021d7af41a68502df39a56030
kgomotsosito <Kg0m0ts01203>
2020-06-08 00:52:29 +0200

    Added subject equals and improved code coverage

--- subject-maintenance/src/test/java/marks/subjectmaintenance/SubjectMaintenanceApplicationTests.java
+++ subject-maintenance/src/test/java/marks/subjectmaintenance/SubjectMaintenanceApplicationTests.java
@@ -3,8 +3,6 @@ package marks.subjectmaintenance;
 import marks.subjectmaintenance.subject.dto.SubjectList;
 import marks.subjectmaintenance.subject.entity.Assessment;
 import marks.subjectmaintenance.subject.entity.Subject;
-import marks.subjectmaintenance.subject.entity.UserSubject;
-import marks.subjectmaintenance.subject.entity.UserSubjectId;
 import marks.subjectmaintenance.subject.service.AssessmentService;
 import marks.subjectmaintenance.subject.service.SubjectService;
 import marks.subjectmaintenance.subject.service.UserSubjectService;
@@ -20,6 +18,7 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
@@ -52,15 +51,15 @@ class SubjectMaintenanceApplicationTests {
 		testSubject.setAssessmentList(listOfAssessment);
 		Subject newSubject = subjectService.createOrUpdate(testSubject);
 
-		assertEquals(testSubject.getCode(), newSubject.getCode());
+		assertTrue(newSubject.equals(testSubject));
 	}
 
 	@Test
 	public void deactivateSubject() {
 		Subject testSubject = new Subject();
 
-		testSubject.setCode("Maths");
-		testSubject.setDescription("Mathematics");
+		testSubject.setCode("Testing");
+		testSubject.setDescription("Subject");
 		testSubject.setActive(Boolean.TRUE);
 		Subject activeSubject = subjectService.createOrUpdate(testSubject);
 		subjectService.deactivateSubject(activeSubject.getId());
@@ -70,44 +69,8 @@ class SubjectMaintenanceApplicationTests {
 	}
 
 	@Test
-	public void findAllSubjects() {
-		SubjectList newSubjects = subjectService.findAllSubjects();
-		assertEquals(newSubjects.getSubjects().size(), newSubjects.getSubjects().size());
-	}
-
-	@Test
 	public void findAllActiveSubjects() {
 		SubjectList newSubjects = subjectService.findAllActiveSubjects();
-		assertEquals(newSubjects.getSubjects().size(), newSubjects.getSubjects().size());
+		assertTrue(newSubjects.getSubjects().stream().allMatch(subject -> subject.getActive().equals(Boolean.TRUE)));
 	}
-
-	@Test
-	public void createAssessment() {
-		Subject subject = subjectService.findBySubjectById(1);
-		Assessment assessment = new Assessment();
-		assessment.setName("CT 2");
-		assessment.setTotal(10);
-		assessment.setDate(new Date());
-		assessment.setSubject(subject);
-
-		List<Assessment> listOfAssessment = new ArrayList<>(Arrays.asList(assessment));
-		subject.setAssessmentList(listOfAssessment);
-		Subject newSubject = subjectService.createOrUpdate(subject);
-
-		assertEquals(newSubject.getAssessmentList().size(), 1);
-	}
-
-	@Test
-	public void createUserSubject() {
-		UserSubjectId userSubjectId = new UserSubjectId(1,1);
-		UserSubject newUserSubject = new UserSubject();
-		newUserSubject.setUserSubjectId(userSubjectId);
-
-		userSubjectService.createOrUpdate(newUserSubject);
-
-		UserSubject userSubject = userSubjectService.findUserSubjectById(userSubjectId);
-
-		assertEquals(newUserSubject.getUserSubjectId(), userSubjectId);
-	}
-
 }
